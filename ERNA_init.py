72365eed8f5235ab4cd86b72bb5d0ddfb5509928
Gabriel Zapodeanu <gzapodea@cisco.com>
2018-01-06 18:03:04 -0800

    work in progress

---
+++ ERNA_init.py
@@ -0,0 +1,18 @@
+
+# developed by Gabi Zapodeanu, TSA, GPO, Cisco Systems
+
+# !/usr/bin/env python3
+
+# This file contains the Spark Auth, Tropo Key, Google Developer Key
+
+SPARK_AUTH = 'Bearer ' + 'Paste your token here'
+
+TROPO_KEY = 'Paste your Tropo key here'
+
+GOOGLE_API_KEY = 'Paste your Google developer key here'
+
+# replace these values with you DNA Center cluster IP, username and password
+
+DNAC_URL = 'https://172.28.97.216'
+DNAC_USER = 'admin'
+DNAC_PASS = 'Cisco123'
