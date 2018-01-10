553fa3b43a9c3cc88abfc4d86ab3c70fa2f3e58a
Gabriel Zapodeanu <gzapodea@cisco.com>
2018-01-09 23:30:28 -0800

    work in progress

--- init.py
+++ init.py
@@ -16,3 +16,16 @@ GOOGLE_API_KEY = 'Paste your Google developer key here'
 DNAC_URL = 'https://172.28.97.216'
 DNAC_USER = 'admin'
 DNAC_PASS = 'Cisco123'
+
+CMX_URL = 'https://172.16.11.27/'
+CMX_USER = 'python'
+CMX_PASSW = 'Clive!17'
+
+ASAv_URL = 'https://10.93.130.40'
+ASAv_USER = 'python'
+ASAv_PASSW = 'cisco'
+
+UCSD_URL = 'https://10.94.132.69'
+UCSD_USER = 'gzapodea'
+UCSD_PASSW = 'cisco.123'
+UCSD_KEY = '1D3FD49A0D474481AE7A4C6BD33EC82E'
